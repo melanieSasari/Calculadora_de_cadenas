2d0effd3e937c8eca88339bf64cfeaa39903e017
melanieSasari <melsasarialborta@gmail.com>
2021-05-04 00:03:41 -0400

    Sexta version

--- spec/calculadora_cadenas_spec.rb
+++ spec/calculadora_cadenas_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "calculadora de cadenas" do
     it "Deberia devolver 8 si la cadena es 6,2" do
         expect(sumar("6,2")).to eq 8
     end
-    it "deberia devolver el numero 15 si le pasamos la cadena 1,2,4,8"do
+    it "Deberia devolver el numero 15 si le pasamos la cadena 1,2,4,8"do
         expect(sumar("1,2,4,8")).to eq 15
     end
     it "Deberia devolver 6 si la cadena es 1\n2,3" do
@@ -17,9 +17,18 @@ RSpec.describe "calculadora de cadenas" do
     it "Deberia devolver 2 si la cadena es 1000,2" do
         expect(sumar("1001,2")).to eq 2
     end
+    it "Deberia devolver el numero 17 si le pasamos la cadena '//[;]\n 6;7;4'" do
+        expect(sumar("//[;]\n6;7;4")).to eq 17
+    end
 end
 def sumar(cadena_numeros)
-    numeros=cadena_numeros.split(/[,\s]/)
+    if (cadena_numeros.split(/\n/)[0].include?"//" )  
+        delimitadores=((cadena_numeros.split(/\n/)[0].gsub"/","").gsub "[","").gsub "]",""
+        numeros=cadena_numeros.split(/\n/)[1].split(Regexp.union(delimitadores.split("")))
+    else
+        numeros=cadena_numeros.split(/[,\s]/)
+    end
     enteros=numeros.map{|i| i.to_i if i.to_i<=1000 }.compact
     suma=enteros.sum()
+    return suma
 end
