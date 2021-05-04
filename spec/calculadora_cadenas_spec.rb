fe591daa388c94744076f882685be177ce096f66
melanieSasari <melsasarialborta@gmail.com>
2021-05-03 22:42:31 -0400

    Primera version

--- spec/calculadora_cadenas_spec.rb
+++ spec/calculadora_cadenas_spec.rb
@@ -1,5 +1,8 @@
 RSpec.describe "calculadora de cadenas" do
-    it "" do
-        
+    it "Deberia devolver 1 si la cadena es 1" do
+        expect(sumar("1")).to eq 1
     end
+end
+def sumar(cadena_numeros)
+    1
 end
