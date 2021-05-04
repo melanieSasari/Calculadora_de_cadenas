396588fb51efad0697c354a47dd042fccd43fb67
melanieSasari <melsasarialborta@gmail.com>
2021-05-03 23:38:23 -0400

    Cuarta version

--- spec/calculadora_cadenas_spec.rb
+++ spec/calculadora_cadenas_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe "calculadora de cadenas" do
     it "Deberia devolver 8 si la cadena es 6,2" do
         expect(sumar("6,2")).to eq 8
     end
+    it "deberia devolver el numero 15 si le pasamos la cadena 1,2,4,8"do
+        expect(sumar("1,2,4,8")).to eq 15
+    end
+    it "Deberia devolver 6 si la cadena es 1\n2,3" do
+        expect(sumar("1\n2,3")).to eq 6
+    end
 end
 def sumar(cadena_numeros)
     numeros=cadena_numeros.split(/[,\s]/)
